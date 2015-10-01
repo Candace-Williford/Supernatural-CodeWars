074484076eb9d752ac719bfc046e5d7b54e5ec85
Candace Williford <candaceaw10@gmail.com>
2015-09-30 20:55:19 -0400

    First commit

---
+++ Supernatural-CodeWars_Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Supernatural_CodeWars.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Behead it with a machete, idjits!", Kata.Bob("vampire"));
+            Assert.AreEqual("It depends on which one it is, idjits!", Kata.Bob("pagan god"));
+            Assert.AreEqual("I have friggin no idea yet, idjits!", Kata.Bob("werepuppy"));
+        }
+    }
+}
